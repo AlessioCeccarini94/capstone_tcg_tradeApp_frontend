03895111b5c8019994f7caef63bbd17ab84c80c2
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-03-22 14:48:39 +0100

    update disclaimer

--- tcgTradeApp/src/components/FooterComponent.jsx
+++ tcgTradeApp/src/components/FooterComponent.jsx
@@ -21,11 +21,11 @@ const Footer = () => {
         <Row className="mt-5">
           <Col>
             <p className="text-center">
-              © 2023 TCG Trade App. All rights reserved. Questo progetto è a
-              scopo puramente didattico. Tutti i contenuti (immagini, marchi,
-              carte) appartengono ai rispettivi proprietari. Non è affiliato né
-              supportato da Wizards of the Coast, Bandai o altri. Nessun dato
-              reale viene utilizzato.
+              © 2023 TCG Trade App. All rights reserved.
+              <br /> Questo progetto è a scopo puramente didattico. Tutti i
+              contenuti (immagini, marchi, carte) appartengono ai rispettivi
+              proprietari. Non è affiliato né supportato da Wizards of the
+              Coast, Bandai o altri. Nessun dato reale viene utilizzato.
             </p>
           </Col>
         </Row>
